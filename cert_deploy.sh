184eaaf980c7576a916740706aaab2ec4de077bb
tkriviradev <tkriviradev@gmail.com>
2017-07-22 20:37:07 +0300

    cert_deploy.sh
    This script is based on official Zimbra guide for Zimbra Letsencrypt Deployment
    This script is tested on Centos7 and ti will install certbot for your from Repository
    
    it's very simple to be used:
    
    1. Download the script > /root/cert_deploy.sh
    2. Download ca_plus_root_ca > /root/ca_plus_root_ca
    3. chmod +x /root/cert_deploy.sh
    4. the the script to crontab
    4.1 30 2 * * 1 /root/cert_renew.sh >> /var/log/le-renew.log
    
    5. Edit the script and modify "getdomains" variable here you have to add all your domains like imap.domain.com smtp.domains.com mail.domain.com etc.
    
    6. Very important all your domains mast be resolvable to the Zimbra Public IP address.

---
+++ cert_deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+servername=$(hostname -f)
+getdomains="-d $servername -d bss.mail.alfafinance.bg"
+
+yum install certbot -y
+
+##########################
+
+/sbin/runuser -l zimbra -c '/opt/zimbra/bin/zmproxyctl stop'
+/sbin/runuser -l zimbra -c '/opt/zimbra/bin/zmmailboxdctl stop'
+
+/usr/bin/certbot certonly --agree-tos --non-interactive --email support@styxsolutions.net --standalone $getdomains
+
+##### fix directory issues #####
+echo "fix directory issues"
+rm -rf /opt/zimbra/ssl/letsencrypt/
+mkdir /opt/zimbra/ssl/letsencrypt/
+
+##### copy needed files to zimbra directory #####
+echo "copy needed files to zimbra directory"
+cp -nf /etc/letsencrypt/live/$servername/*.pem /opt/zimbra/ssl/letsencrypt/
+
+##### modify chain.pem file #####
+echo "modify chain.pem file"
+cat /root/ca_plus_root_ca >> /opt/zimbra/ssl/letsencrypt/chain.pem
+
+### Than to give zimbra rights to the directory ###
+chown -R zimbra.zimbra /opt/zimbra/ssl/letsencrypt/*
+
+### Now we have to verify the Certificate #####
+echo "Now we have to verify the Certificate"
+cd /opt/zimbra/ssl/letsencrypt/ && /opt/zimbra/bin/zmcertmgr verifycrt comm privkey.pem cert.pem chain.pem
+cp -nf /opt/zimbra/ssl/letsencrypt/privkey.pem /opt/zimbra/ssl/zimbra/commercial/commercial.key
+
+### Now let's install the Certificate #####
+echo "we are installing the certificate"
+cd /opt/zimbra/ssl/letsencrypt/ && /opt/zimbra/bin/zmcertmgr deploycrt comm cert.pem chain.pem
+
+### Now let's Restart the Zimbra Server #####
+echo "Server Restart"
+/sbin/runuser -l zimbra -c '/opt/zimbra/bin/zmcontrol restart'
+
+exit
